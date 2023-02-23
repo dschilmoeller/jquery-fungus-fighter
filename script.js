9d7b272a13d483c28235235c2322e9c7fd3447c7
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-02-23 13:42:41 -0600

    added functionality to render to kill fungus. comment cleanup.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $(document).ready(onReady);
-console.log('link JQ test')
+
+//console.log('link JQ test')
+
 // State Variables can be declared outside of the onReady
 // Feel free to make this to what you want!
 // Example:
@@ -23,38 +25,46 @@ function onReady() {
     // handle click on entangle
     // handle click on dragon blade
     // handle click on star fire
+}
 
 function attack(attack) {
     // console.log(`You're attacking with: `, attack);
     // use scepter!
     if (attack === 'sceptre') {
         // console.log(`You're attacking with: `, attack);
+        // reduce AP
+        playerAP += -12;
+        console.log('player ap is', playerAP);
+        // reduce HP
+        fungusAmongUs += -14;
+        console.log('fungus hp is', fungusAmongUs);
+        renderAPHP();
     }
     if (attack === 'entangle') {
         // console.log(`You're attacking with: `, attack);
+        playerAP += -23;
+        console.log('player ap is', playerAP);
+        fungusAmongUs += -9;
+        console.log('fungus hp is', fungusAmongUs);
+        renderAPHP();
     }
     if (attack === 'dragon') {
         // console.log(`You're attacking with: `, attack);
+        playerAP += -38;
+        console.log('player ap is', playerAP);
+        fungusAmongUs += -47;
+        console.log('fungus hp is', fungusAmongUs);
+        renderAPHP();
     }
     if (attack === 'star') {
         // console.log(`You're attacking with: `, attack);
-    }
+        playerAP += -33;
+        console.log('player ap is', playerAP);
+        fungusAmongUs += -25;
+        console.log('fungus hp is', fungusAmongUs);
+        renderAPHP();
+    } 
 }
-// function to use scepter
-    // check sufficient ap
-    // reduce AP
-    // reduce HP
-    // check fungus status -> kill if dead
-    // check AP status ->
-        // if negative change to 0
-        // fungus jump if 0
-
-
-// function to use entangle
-
-// function to use dragon blade
-
-// function to use star fire
 
 // function to kill fungus
     
@@ -62,7 +72,36 @@ function attack(attack) {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+
+
+function renderAPHP() {
+    console.log('rendering AP/HP');
+    // replace AP value
+    let APOnDOM = $('#ap-text-id');
+    console.log('render: player AP as', playerAP)
+    console.log('render: fungus HP as:', fungusAmongUs)
+    APOnDOM.empty();
+    APOnDOM.append( playerAP, ' AP' );
+    // check AP Value
+    if ( playerAP < 1 ) {
+        console.log('Yer outta AP!');
+        playerAP = 0; // this doesn't seem to be working. 
+        $(.)
+    }
+    // replace HP value
+    let HPOnDOM = $('#hp-text-id');
+    HPOnDOM.empty();
+    HPOnDOM.append( fungusAmongUs, ' HP' );
+
+    // check HP value
+    if ( fungusAmongUs < 1 ) {
+        console.log('Fungus is down!')
+        fungusAmongUs = 0; // this doesn't seem to be working.
+        // change walk class to dead class
+        $('.freaky-fungus').addClass('dead')
+    }
 }
+
 // Arcane Scepter
 // AP Cost: 12 / HP Damage: 14
 // Entangle
@@ -71,15 +110,3 @@ function attack(attack) {
 // AP Cost: 38 / HP Damage: 47
 // Star Fire
 // AP Cost: 33 / HP Damage: 25
-
-function renderAPHP() {
-    console.log('rendering AP/HP');
-    // replace AP value
-    let APOnDOM = $('#ap-text-id');
-    APOnDOM.empty();
-    APOnDOM.append( 100, ' AP' );
-    // replace HP value
-    let HPOnDOM = $('#ap-text-id');
-    HPOnDOM.empty();
-    HPOnDOM.append( 100, ' AP' );
-}
